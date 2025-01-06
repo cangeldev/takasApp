1472d0a0d93c7847425a05fdc4680fba17bb9bd8
Can Gel <84725623+cangeldev@users.noreply.github.com>
2025-01-06 15:47:32 +0300

    Absolute Path added.

---
+++ src/components/test/test.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const Test = () => {
+    return (
+        <View>
+            <Text>test</Text>
+        </View>
+    )
+}
